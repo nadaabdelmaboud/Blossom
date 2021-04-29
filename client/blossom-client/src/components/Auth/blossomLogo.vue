73676eaa7ca71b4b35885f3afe829a2d1e7aa438
Menna Mahmoud <menna123mahmoud@gmail.com>
2021-04-30 01:52:37 +0200

    common styling and logo

---
+++ client/blossom-client/src/components/Auth/blossomLogo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="logo">
+    <img src="../../assets/BlossomLogo_v7.png" alt="logo Image" />
+    <p class="slogan">Give it time. I’ll grow on you.</p>
+    <p />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {},
+  data: function () {
+    return {};
+  },
+  computed: {},
+  created: function () {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../scss/Colors";
+@import "../../scss/GlobalPopup";
+@import "../../scss/General";
+.logo {
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+}
+img {
+  width: 250px;
+  display: flex;
+  align-self: center;
+}
+</style>
---
+++ client/blossom-client/src/views/AuthPopup.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="transparentBackground">
+    <div class="popupBody">
+      <LoginForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from "../components/Auth/loginForm";
+export default {
+  name: "Authentication",
+  components: {
+    LoginForm,
+  },
+  data: function () {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/Colors";
+@import "../scss/GlobalPopup";
+.popupBody {
+  width: 900px;
+  padding: 0px;
+}
+</style>
