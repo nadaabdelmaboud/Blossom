c211a89a8469946d209a8638bc2d87d382661501
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2021-05-01 22:18:46 +0200

    Edit Middlewares

--- server/app/routes/user.route.js
+++ server/app/routes/user.route.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const UserController = require('../controllers/user.controller');
-router.get("/users/", UserController.getAllUsers);
-router.get("/users/:id", UserController.getUser);
-router.put("/users/:id", UserController.updateUser);
-router.delete("/users/:id", UserController.deleteUser);
+const AuthMiddleware = require('../middlewares/auth.middleware') 
+const AdminMiddleware = require('../middlewares/admin.middleware') 
+
+router.get("/users/",AuthMiddleware,AdminMiddleware, UserController.getAllUsers);
+router.get("/users/:id",AuthMiddleware, UserController.getUser);
+router.put("/users/:id",AuthMiddleware, UserController.updateUser);
+router.delete("/users/:id",AuthMiddleware, UserController.deleteUser);
 module.exports = router;
